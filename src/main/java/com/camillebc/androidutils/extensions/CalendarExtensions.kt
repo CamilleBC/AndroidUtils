621e7529101210a81860a99b83e57ca934c9c3c0
Camille Baillat <camille.baillat@pm.me>
2019-03-27 18:11:46 +0100

    [extensions] Add some Calendar extensions
    add a file to gitignore and some Calendar extensions.

--- src/main/java/com/camillebc/androidutils/extensions/CalendarExtensions.kt
+++ src/main/java/com/camillebc/androidutils/extensions/CalendarExtensions.kt
@@ -1,8 +1,21 @@
 package com.camillebc.androidutils.extensions
 
+import java.lang.StringBuilder
 import java.text.SimpleDateFormat
 import java.util.*
 
+const val CALENDAR_SEP = " | "
+
+fun Calendar.combinedFormat(): SimpleDateFormat = SimpleDateFormat("EEE, d MMM yyyy" + CALENDAR_SEP + "HH:mm")
+fun Calendar.combinedStrings(dateString: String, timeString: String) = StringBuilder()
+    .append(dateString)
+    .append(CALENDAR_SEP)
+    .append(timeString)
+    .toString()
 fun Calendar.dateFormat(): SimpleDateFormat = SimpleDateFormat("EEE, d MMM yyyy")
 fun Calendar.dateString(): String = this.dateFormat().format(this.time).capitalize()
 fun Calendar.dateStringToDate(dateString: String) = dateFormat().parse(dateString)
+fun Calendar.stringToDate(dateString: String, timeString: String) = combinedFormat().parse(combinedStrings(dateString, timeString))
+fun Calendar.timeFormat(): SimpleDateFormat = SimpleDateFormat("HH:mm")
+fun Calendar.timeString(): String = this.timeFormat().format(this.time).capitalize()
+fun Calendar.timeStringToDate(timeString: String) = timeFormat().parse(timeString)
